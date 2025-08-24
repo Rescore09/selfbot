d5ac8a1838b843baa8d9e95273b58c328ffe6f91
Rescore <49985481+Rescore09@users.noreply.github.com>
2025-08-23 16:01:46 -1200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,5 +4,6 @@ echo Installing Python dependencies...
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo Setup completed. You can now run the bot using start.bat
+echo Setup completed.
 pause
+
